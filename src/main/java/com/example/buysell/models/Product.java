f398966710a26a7bbd8d8dc11a4064209c1f29d8
ParubovDaniil <ParubovDaniil@mail.ru>
2023-11-14 16:02:31 +0500

    Second step

--- src/main/java/com/example/buysell/models/Product.java
+++ src/main/java/com/example/buysell/models/Product.java
@@ -2,14 +2,28 @@ package com.example.buysell.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity //указываем на то что класс является тем классом,который эмулирует таблицу из базы данных
+@Table(name = "products") //название таблицы
 @Data
 @AllArgsConstructor//ЧТо бы был конструктор со всеми полями которые присутствуют
+@NoArgsConstructor //
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "description",columnDefinition = "text")//для того что бы колонка не была типа varchar а была типа text
     private String description;
+    @Column(name = "price")
     private int price;
+    @Column(name = "city")
     private String city;
+    @Column(name = "author")
     private String author;
 }
--- src/main/java/com/example/buysell/services/ProductService.java
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -1,33 +1,36 @@
 package com.example.buysell.services;
 
 import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service //является компонетном
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
-    {
-        products.add(new Product(++ID,"PlayStation 5 ","Simple description",67000,"Ekaterinburg","Daniil"));
-        products.add(new Product(++ID,"Iphone 8 ","Simple description",24000,"Moscow","Kolya"));
+
+    private final ProductRepository productRepository;
+
+    public List<Product> listProducts(String title) {
+        if(title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-    public List<Product> listProducts(){return products;}
+    public void saveProduct(Product product) {
+        log.info("Saving new {}",product);
+        productRepository.save(product);
+    }
 
-    public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
-    public void deleteProduct(Long id){
-        products.removeIf(product -> product.getId().equals(id));
-    }
-    public Product getProductById(Long id){
-        for (Product product : products) {
-            if(product.getId().equals(id)) return product;
-        }
-        return null;
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
     }
 }
